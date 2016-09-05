d6580ad5142ef055b3f18bb273e1cc8d67108f72
super-programmer <593655843@qq.com>
2016-09-05 12:47:07 +0800

    dfsd

--- js/rem.js
+++ js/rem.js
@@ -13,7 +13,6 @@
                 docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
             }
         };
-
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
